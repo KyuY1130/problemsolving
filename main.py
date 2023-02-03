0f7650dcf0ffb9322245945eff9157bd62c8c639
KyuY1130 <106563260+KyuY1130@users.noreply.github.com>
2023-02-03 07:35:47 +0000

    solving

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-t= int(input())
-for i in range(t):
-  k=int(input())
-  n=int(input())
-  b=[]
-  for i in range(n):
-      b.append(i+1)
-  for i in range(k):
-    a=[]
-    sum=0
-    for i in b:
-      sum+=i
-      a.append(sum)
-    b=a
-  print(b[-1])
-    
+N=int(input())
+e=0
+if N//5==0:
+  if N==3:
+    e=1
+  else:
+    e=-1
+else:
+    if N%5==0:
+      e=N//5
+    else:
+      for i in range(N//5,-1,-1):
+        if (N-5*i)%3==0:
+          e=i+(N-5*i)//3
+          break
+        elif N%3!=0:
+          e=-1
+print(e)
+
+s= int(input())
+bag=0
+while bag>=0
+  if s %5==0
+    bag+=(s//5)
+    print(bag)
+    break
+  sugar-=3
+  bag+=1
+else:
+  print(-1)
