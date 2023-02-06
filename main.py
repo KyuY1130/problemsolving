c0ecccd5f750ff48e62932bc26718f302f9ae123
KyuY1130 <106563260+KyuY1130@users.noreply.github.com>
2023-02-06 14:43:12 +0000

    solving

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-N= int(input())
-if N>1:
-  for i in range(2,N+1):
-    if N%i==0:
-      while N%i==0:
-        print(i)
-        N/=i
+from math import sqrt
+while True:
+  n=int(input())
+  if n==0:
+    break
+  sosu=0
+  for i in range(n+1,(2*n)+1):
+    if i==1:
+      continue
+    for j in range(2,int(sqrt(i)+1)):
+      if i%j==0:
+        break
+    else:
+      sosu+=1
+  print(sosu)
+
+https://ji-gwang.tistory.com/218
